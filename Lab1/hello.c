72840c7474847d5e2193497cb208c8f0d5cf9500
hushi <hushi@indiana.edu>
2016-01-21 13:40:19 -0500

    Rediting the lab report

--- Lab1/hello.c
+++ Lab1/hello.c
@@ -1,9 +1,17 @@
+/* Hello.c
+ * Author: Shichao Hu
+ * Date Created: 1/14/2016
+ * Last Modified by: Shichao Hu
+ * Date Last Modified: 1/14/2016
+ * Lab1
+ */
+
 #include<stdio.h>
 #include<unistd.h>
 
 void main(){
 	int i = 1;
-	while(i=1){
+	while(i=1){ //while loop, when i = 1, keep looping
 		printf("Hello world\n");
 		sleep(3);
 	}
