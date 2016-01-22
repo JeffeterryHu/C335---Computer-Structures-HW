10a2da5889d16725de668ee6ab19b53578fc3290
Shichao Hu <hushi@cottonmouth.soic.indiana.edu>
2016-01-21 19:12:54 -0500

    uploading Lab2

--- Lab1/icebreaker.c
+++ Lab1/icebreaker.c
@@ -1,16 +1,29 @@
+/* icebreaker.c
+ * Author: Shichao Hu
+ * Date Created: 1/14/2016
+ * Last Modified by: Shichao Hu
+ * Date Last Modified: 1/14/2016
+ * Partner: Yangjun Liu
+ * Lab1
+ */
+
 #include<stdio.h>
 #include<unistd.h>
 
 void main(){
 	int i = 1;
-	while(1){
+	while(1){ //while it is true, keep looping
 		printf("Hello world\n");
 		char name[100];
 		FILE *f;
-		sprintf(name, "../../Desktop/%d.txt", i);
+
+		sprintf(name, "../../Desktop/%d.txt", i);  
+		//convert the number to char using sprintf
+
 		f = fopen(name,"w");
 		fclose(f);
+
 		i++;
-		sleep(1);
+		sleep(3);
 	}
 }
