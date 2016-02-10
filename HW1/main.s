96f2d40edc025690a93dfa12e3a9a18dd8a563f5
hushi <hushi@indiana.edu>
2016-02-09 23:45:30 -0500

    newer version of HW1

---
+++ HW1/main.s
@@ -0,0 +1,33 @@
+	.file	"main.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	jmp	.L2
+.L3:
+	movl	$0, %eax
+	call	stop_light
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	movl	$0, %eax
+	call	inc
+	movl	%eax, -4(%rbp)
+.L2:
+	cmpl	$35, -4(%rbp)
+	jle	.L3
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-4)"
+	.section	.note.GNU-stack,"",@progbits
